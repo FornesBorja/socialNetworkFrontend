0bae6ce8f769efa6e293785e51df856ff2aa4527
“FornesBorja” <fornes.borja@gmail.com>
2024-08-11 23:43:50 +0200

    wip: header in progress added logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { Surfer } from '../Surfer/Surfer';
+import "./Header.css"
 
 export const Header = () => {
+  const navigate = useNavigate();
+
+  const passport = JSON.parse(localStorage.getItem("passport"));
+  let token, role;
+  if (passport) {
+    token = passport.token;
+    role = passport.tokenData.roleId;
+  }
   return (
-    <div>Header</div>
+    <div>
+      <div id="navbar" className='germania-one'>
+        <Surfer path="/" classAdd="jacquard-24" >
+        </Surfer>
+        <div className="right-section">
+        {token ? (
+          <>
+            <div className="log-button" onClick={() => {(localStorage.removeItem("passport")); navigate("/login")}}> LOGOUT </div>
+          </>
+        ) : (
+          <>
+            <Surfer classAdd="log-button" path="/register" content="Register" />
+            <Surfer classAdd="log-button" path="/login" content="Login" />
+          </>
+        )}        
+        </div>
+      </div>
+    </div>
   )
 }
