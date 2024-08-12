9a3a880015842cd7e68da1138289ca6ee76f85f9
“FornesBorja” <fornes.borja@gmail.com>
2024-08-12 18:57:57 +0200

    feat: style added to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
-import { Surfer } from '../Surfer/Surfer';
-import "./Header.css"
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Surfer } from "../Surfer/Surfer";
+import "./Header.css";
 
 export const Header = () => {
   const navigate = useNavigate();
@@ -14,22 +14,34 @@ export const Header = () => {
   }
   return (
     <div>
-      <div id="navbar" className='germania-one'>
-        <Surfer path="/" classAdd="jacquard-24" >
-        </Surfer>
+      <div id="navbar" className="germania-one">
+        <Surfer path="/" classAdd="jacquard-24" content="Social Network" />
         <div className="right-section">
-        {token ? (
-          <>
-            <div className="log-button" onClick={() => {(localStorage.removeItem("passport")); navigate("/login")}}> LOGOUT </div>
-          </>
-        ) : (
-          <>
-            <Surfer classAdd="log-button" path="/register" content="Register" />
-            <Surfer classAdd="log-button" path="/login" content="Login" />
-          </>
-        )}        
+          {token ? (
+            <>
+              <div
+                className="log-button"
+                onClick={() => {
+                  localStorage.removeItem("passport");
+                  navigate("/login");
+                }}
+              >
+                {" "}
+                LOGOUT{" "}
+              </div>
+            </>
+          ) : (
+            <>
+              <Surfer
+                classAdd="log-button"
+                path="/register"
+                content="Register"
+              />
+              <Surfer classAdd="log-button" path="/login" content="Login" />
+            </>
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
