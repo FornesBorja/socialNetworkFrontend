420990c51c35c6027c34142e09847b0da6c0ac0e
“FornesBorja” <fornes.borja@gmail.com>
2024-08-19 01:29:16 +0200

    feat: content added to home page body

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -5,8 +5,14 @@ export const Home = () => {
   return (
     <div id="container">
       <div id="content">
-      <div id="container-left">a</div>
-      <div id="container-right">a</div>
+      <div id="container-left">
+        <div>Welcome to social network</div>
+        <img src="https://user-images.githubusercontent.com/46146748/63127038-62273800-bf7f-11e9-914f-bd1c431c76f2.png"/>
+      </div>
+      <div id="container-right">
+        <button>Register</button>
+        <button>Log in</button>
+      </div>
       </div>
     </div>
   );
