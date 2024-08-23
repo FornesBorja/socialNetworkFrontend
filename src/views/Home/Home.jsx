52f5561d7591e9f6d7d5db00346810f4119c467d
FornesBorja <fornes.borja@gmail.com>
2024-08-23 23:46:25 +0200

    feat: input custom (button) added at homeç

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import "./Home.css";
 import { useNavigate } from "react-router-dom";
+import { Input } from "../../components/Input/Input";
 
 export const Home = () => {
   const navigate = useNavigate();
+
+  const navigateURL = (place) =>{
+    navigate("/"+place)
+  }
   return (
     <div id="container">
       <div id="content">
@@ -12,8 +17,8 @@ export const Home = () => {
         <img src="https://user-images.githubusercontent.com/46146748/63127038-62273800-bf7f-11e9-914f-bd1c431c76f2.png"/>
       </div>
       <div id="container-right">
-        <button onClick={() => navigate("/register")}>Register</button>
-        <button>Log in</button>
+        <Input type="button" name="register-button" value="Register" className="auth-button" click={() => navigateURL("register")}></Input>
+        <Input type="button" name="login-button" value="Login" className="auth-button" click={() => navigateURL("login")}></Input>
       </div>
       </div>
     </div>
