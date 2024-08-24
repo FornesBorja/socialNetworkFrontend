bf90f85deb274f266a291b2b60492e6edb3fc292
FornesBorja <fornes.borja@gmail.com>
2024-08-24 13:06:13 +0200

    feat: css structure for profile

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -13,7 +13,7 @@ export const Home = () => {
     <div id="container">
       <div id="content">
       <div id="container-left">
-        <div>Welcome to social network</div>
+        <div className="welcome-message">Welcome to social network</div>
         <img src="https://user-images.githubusercontent.com/46146748/63127038-62273800-bf7f-11e9-914f-bd1c431c76f2.png"/>
       </div>
       <div id="container-right">
--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Input } from "../../components/Input/Input";
+import "./Profile.css";
 
 export const Profile = () => {
   return (
@@ -10,21 +11,30 @@ export const Profile = () => {
             <img src="https://imgs.search.brave.com/guFeeYHxGB1sxa5fjRQz35X6MNFk-_AmM_fj6PNO1X0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/ZnJlZS1waG90by91/c2VyLXByb2ZpbGUt/aWNvbi1mcm9udC1z/aWRlXzE4NzI5OS0z/OTU5Ni5qcGc_c2l6/ZT02MjYmZXh0PWpw/Zw" />
           </div>
           <div className="left-bottom">
-            <div className="following"></div>
-            <div className="followers"></div>
+            <div className="following">
+              <div>Following</div>
+              <div className="number-following">0</div>
+            </div>
+            <div className="followers">
+              <div>Followers</div>
+              <div className="number-followers">0</div>
+            </div>
           </div>
         </div>
         <div id="container-right">
           <div className="right-top">
-            <Input
-              name="edit-profile-button"
-              type="button"
-              className="button-general"
-              value="Edit profile"
-            />
+            <div className="name-profile">ejemplo@gmail.com</div>
+            <div className="edit-profile">
+              <Input
+                name="edit-profile-button"
+                type="button"
+                className="button-general"
+                value="Edit profile"
+              />
+            </div>
           </div>
           <div className="right-bottom">
-            <div className="post-container"></div>
+            <div className="post-container">d</div>
           </div>
         </div>
       </div>
