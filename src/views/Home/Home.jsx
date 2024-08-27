b1cbbec85918e4bfbc6f9948888451647e0cdbfa
FornesBorja <fornes.borja@gmail.com>
2024-08-27 16:54:46 +0200

    feat: changes in profile.css

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -12,11 +12,11 @@ export const Home = () => {
   return (
     <div id="container">
       <div id="content">
-      <div id="container-left">
+      <div className="container-left">
         <div className="welcome-message">Welcome to social network</div>
         <img src="https://user-images.githubusercontent.com/46146748/63127038-62273800-bf7f-11e9-914f-bd1c431c76f2.png"/>
       </div>
-      <div id="container-right">
+      <div className="container-right">
         <Input type="button" name="register-button" value="Register" className="auth-button" click={() => navigateURL("register")}></Input>
         <Input type="button" name="login-button" value="Login" className="auth-button" click={() => navigateURL("login")}></Input>
       </div>
