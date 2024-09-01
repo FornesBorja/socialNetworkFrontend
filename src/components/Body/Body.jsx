efd284413b6b78884f9288fca8edd600fa385e72
“FornesBorja” <fornes.borja@gmail.com>
2024-09-01 12:47:47 +0200

    feat: now path / redirects to /feed if a user is logged

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,21 +1,25 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { NotFound } from '../../views/NotFound/NotFound';
+import { Routes, Route, Navigate } from "react-router-dom"; 
+import { NotFound } from "../../views/NotFound/NotFound";
 import { Home } from "../../views/Home/Home";
 import { Register } from "../../views/Register/Register";
 import { Login } from "../../views/Login/Login";
 import { Profile } from "../../views/Profile/Profile";
-
+import { Feed } from "../../views/Feed/Feed";
 
 export const Body = () => {
+  const isLoggedIn = () => {
+    return !!localStorage.getItem("passport");
+  };
   return (
     <>
       <Routes>
         <Route path="*" element={<NotFound />} />
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={isLoggedIn() ? <><Navigate to="/feed" /></>: <Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/feed" element={<Feed />} />
       </Routes>
     </>
   );
