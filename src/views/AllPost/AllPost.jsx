6a65c60e373aa839a34ca7340948ad7eed8f2ae2
“FornesBorja” <fornes.borja@gmail.com>
2024-09-01 16:38:37 +0200

    feat: feed selection between all posts and following

---
+++ src/views/AllPost/AllPost.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AllPost = () => {
+  return (
+    <div className="view">
+      <h1>All posts</h1>
+    </div>
+  );
+};
+
+export default AllPost;
--- src/views/Feed/Feed.jsx
+++ src/views/Feed/Feed.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState } from "react";
+import TopBar from "../../components/TopBar/TopBar";
+import Following from "../Following/Following";
+import AllPost from "../allPost/allPost";
+import "./Feed.css"
+
 
 export const Feed = () => {
+  const [activeTab, setActiveTab] = useState("following");
+
+  const handleTabChange = (tab) => {
+    setActiveTab(tab);
+  };
   return (
-    <div>Feed</div>
-  )
-}
+    <div className="app">
+      <TopBar  activeTab={activeTab} onTabChange={handleTabChange} />
+      {activeTab === 'allPost' && <AllPost />}
+      {activeTab === 'following' && <Following />}
+    </div>
+  );
+};
